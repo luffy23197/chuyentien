d3c71d01497bbc93af70fe8adf1ed7f5d2bb1e21
luffy23197 <kintarou231@gmail.com>
2025-08-11 17:12:42 +0700

    Delete udmt.js

--- udmt.js
+++
@@ -1,9 +0,0 @@
-function comvert() : void {}
-let money = document.getElementById("money").value;
-let from = document.getElementById("from").value;
-let to = document.getElementById("to").value;
-
-// ct chuyen doi: s = money * to /from
-let result : number = money * to /from
-// output
-document.getElementById("result").innerHTML = result;
